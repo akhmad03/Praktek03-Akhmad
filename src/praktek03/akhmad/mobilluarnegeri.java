154a3a4f5ef97b8d601285740f392743348042b3
akhmad <>
2018-04-08 23:00:10 +0800

    atribut dan method

--- src/praktek03/akhmad/mobilluarnegeri.java
+++ src/praktek03/akhmad/mobilluarnegeri.java
@@ -3,5 +3,10 @@ package praktek03.akhmad;
 
 
 public class mobilluarnegeri extends mobil {
+    private String negara;
     
+    void throttle (){
+        System.out.println("JEPANG......");
+    
+    }
 }
