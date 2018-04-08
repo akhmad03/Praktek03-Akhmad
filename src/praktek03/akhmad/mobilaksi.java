5789a118a26ea78ec54fecd1a75f14d0ae2ab65a
akhmad <>
2018-04-08 22:40:40 +0800

    Instance, Membuat Attribut dan panggil Method

--- src/praktek03/akhmad/mobilaksi.java
+++ src/praktek03/akhmad/mobilaksi.java
@@ -4,7 +4,14 @@ package praktek03.akhmad;
 
 public class mobilaksi {
     public static void main(String[] args) {
+        mobil r = new mobil();
+        r.merk="Fortuner";
+        r.jenis="SUV";
+        r.harga=1000;
         
+         
+
+        r.cetakInfo();
     }
     
 }
